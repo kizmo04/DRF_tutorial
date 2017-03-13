1816d1ad8e93f1aa1799fb4ce8a641e195731488
kizmo04 <kizmo04@gmail.com>
2017-03-13 23:12:02 +0900

    snippets 앱 생성, INSTALLED_APPS에 snippets, rest_framework 추가

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -51,6 +51,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'snippets',
 ]
 
 MIDDLEWARE = [
