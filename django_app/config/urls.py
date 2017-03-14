a869e4274a213de34adc7af2bec7945728cc0b0f
kizmo04 <kizmo04@gmail.com>
2017-03-14 17:37:09 +0900

    url snippets과 users로 분리

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('snippets.urls')),
+    url(r'^snippets/', include('snippets.urls')),
+    url(r'^users/', include('member.urls')),
 ]
--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.SnippetList.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^$', views.UserList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
