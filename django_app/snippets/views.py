77ca66c2883faf2c485c826fbc861adc50cd91a7
kizmo04 <kizmo04@gmail.com>
2017-03-14 14:59:54 +0900

    url수정, snippet_list 구현

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+class JSONResponse(HttpResponse):
+    """
+    콘텐츠를 JSON으로 변환한 후 HttpResponse 형태로 반환합니다.
+    """
+
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request) # data에 파이썬 객체형식으로 반환된 데이터를 할당
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
