079880ebca3779f1711f8edefd420003a83c151d
kizmo04 <kizmo04@gmail.com>
2017-03-14 16:59:59 +0900

    클래스 기반 뷰로 API 재작성

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -50,6 +50,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework_xml.renderers.XMLRenderer',
     ),
 }
