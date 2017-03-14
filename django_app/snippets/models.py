62e058e7c5cdf8fa4c01d28f2f9bdc4b3263d59c
kizmo04 <kizmo04@gmail.com>
2017-03-14 17:22:38 +0900

    모델에 속성 추가하고 migrations추가

--- django_app/snippets/models.py
+++ django_app/snippets/models.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -14,6 +17,20 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='snippets')
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ('created',)
+
+    def save(self, *args, **kwargs):
+        """
+        `pygments` 라이브러리를 사용하여 하이라이트된 코드를 만든다.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = self.linenos and 'table' or False
+        options = self.title and {'title': self.title} or {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
