35057e1d6c58ae735a940554de7c588cd6ad9376
kizmo04 <kizmo04@gmail.com>
2017-03-14 15:37:43 +0900

    url 수정

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^snippets/', include('snippets.urls')),
+    url(r'^', include('snippets.urls')),
 ]
