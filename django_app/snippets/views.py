d0d3760e375cce98f2424b390d045b95dbcefdf0
kizmo04 <kizmo04@gmail.com>
2017-03-14 15:33:00 +0900

    snippets_detail 구현 url 수정

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -29,9 +29,36 @@ def snippet_list(request):
         return JSONResponse(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request) # data에 파이썬 객체형식으로 반환된 데이터를 할당
+        data = JSONParser().parse(request)  # data에 파이썬 객체형식으로 반환된 데이터를 할당
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JSONResponse(serializer.data, status=201)
         return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()  # 업데이트가 일어난다.
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
