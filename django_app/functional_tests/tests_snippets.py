4e34f9885d2de2066f4d9587cc9c0ad4872c3b97
kizmo04 <kizmo04@gmail.com>
2017-03-15 14:49:41 +0900

    create_snippet헬퍼 함수 이용한 snippet_list 테스트 작성

--- django_app/functional_tests/tests_snippets.py
+++ django_app/functional_tests/tests_snippets.py
@@ -1,6 +1,10 @@
+import random
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APILiveServerTestCase
+
+from snippets.models import Snippet
 
 
 class SnippetTest(APILiveServerTestCase):
@@ -24,16 +28,27 @@ class SnippetTest(APILiveServerTestCase):
                 return response
 
     def test_snippet_list(self):
-        pass
+        num = random.randrange(1, 20)
+        self.create_snippet(num)
+        self.assertEqual(Snippet.objects.count(), num)
+        # values_list를 사용해서 필요한 항목만 가져올 경우
+        for index, snippet_value_tuple in enumerate(Snippet.objects.values_list('title', 'code')):
+            self.assertEqual(snippet_value_tuple[0], self.test_title.format(index + 1))
+            self.assertEqual(snippet_value_tuple[1], self.test_code.format('!' * (index + 1)))
+
+        # 쿼리가 매우 커서 일부만 사용할 경우
+        for index, snippet in enumerate(Snippet.objects.all().iterator()):
+            self.assertEqual(snippet.title, self.test_title.format(index + 1))
+            self.assertEqual(snippet.code, self.test_code.format('!' * (index + 1)))
 
     def test_snippet_create(self):
         response = self.create_snippet()
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED )
-        self.assertEqual(response.data.get('title'), self.test_title.format(1) )
-        self.assertEqual(response.data.get('code'), self.test_code.format('!') )
-        self.assertEqual(response.data.get('linenos'), self.default_linenos )
-        self.assertEqual(response.data.get('language'), self.default_language )
-        self.assertEqual(response.data.get('style'), self.default_style )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get('title'), self.test_title.format(1))
+        self.assertEqual(response.data.get('code'), self.test_code.format('!'))
+        self.assertEqual(response.data.get('linenos'), self.default_linenos)
+        self.assertEqual(response.data.get('language'), self.default_language)
+        self.assertEqual(response.data.get('style'), self.default_style)
 
     def test_snippet_retrieve(self):
         pass
