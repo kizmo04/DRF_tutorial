610f00fc08a82b727f725a43fb916724518c881b
kizmo04 <kizmo04@gmail.com>
2017-03-15 15:50:14 +0900

    member url을 바꿈

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^snippets/', include('snippets.urls')),
-    url(r'^users/', include('member.urls')),
+    url(r'^member/', include('member.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
